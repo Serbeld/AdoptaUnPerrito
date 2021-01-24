04269e23f6eeb98477db59b1da9e6b7bb6926517
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2021-01-24 18:10:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ $(".botonPopUp").click(
         $(".ContenedorPopUp").hide()
         $(".Ops-error").show()
         $(".Pantalla-gris").hide()
+        $(".swiper-container").hide()
     }
 )
 
@@ -44,7 +45,7 @@ for (var i = 0; i < 12; i++){
 	document.querySelector(".sku-dog-data").value = numero;
 	});
 	div.tabIndex = i+1;
-	div.classList.add("flex-item", `Number:${numero}`, `Name:${nombresp[numero]}`);
+	div.classList.add("flex-item", `Value-sku:${numero}`, `Name:${nombresp[numero]}`);
 	div.innerHTML += perrito[0] + perrito[1] + perrito[2] + perrito[3];
 	documentFragment.appendChild(div);
 }
