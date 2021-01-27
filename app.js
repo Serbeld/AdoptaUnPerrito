7cea0cefdf0a0ec6dfc7b089f7f7657ad961be7a
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2021-01-27 00:43:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ contenedor.appendChild(documentFragment);
 var swiper = new Swiper('.swiper-container', {
 	slidesPerView: 1,
 	loop: true,
-	speed:1800,
-	autoplay: {delay: 5000},
+	speed:1000,
+	autoplay: {delay: 3000},
 	pagination: {
 		el: '.swiper-pagination',
 		type: 'progressbar',
